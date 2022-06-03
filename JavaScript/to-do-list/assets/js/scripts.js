89b8fa97446300c4460a82e375b1d7da0460ed92
Neto002 <antoniogfneto92@hotmail.com>
2022-06-03 16:02:56 -0300

    to-do list javascript

--- JavaScript/to-do-list/assets/js/scripts.js
+++ JavaScript/to-do-list/assets/js/scripts.js
@@ -1,14 +1,20 @@
-var inputTarefa = document.getElementById('inputTarefa')
-
 function criaTarefa() {
-    var tarefa = inputTarefa.value
-    if (tarefa == '') {
-        alert('Digite uma tarefa')
-    } else if (tarefa.length > 0) {
-        var lista = document.getElementById('lista')
-        var item = document.createElement('li')
-        item.innerHTML = tarefa
-        lista.appendChild(item)
-        inputTarefa.value = ''
+    var li = document.createElement("li")
+    var inputTarefa = document.getElementById("inputTarefa").value
+    var tarefa = document.createTextNode(inputTarefa)
+    li.appendChild(tarefa)
+
+    if (inputTarefa === '') {
+        alert("Por favor, preencha o campo.")
+    } else {
+        document.getElementById("listaTarefas").appendChild(li)
     }
+    document.getElementById("inputTarefa").value = ""
 }
+
+var listaTarefas = document.getElementById("listaTarefas")
+listaTarefas.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked')
+    }
+}, false)
