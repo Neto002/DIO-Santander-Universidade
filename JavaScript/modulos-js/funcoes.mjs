cd3d702f15fe01e012593590aece126e1104ed83
Neto002 <antoniogfneto92@hotmail.com>
2022-06-06 11:16:39 -0300

    módulos em javascript

---
+++ JavaScript/modulos-js/funcoes.mjs
@@ -0,0 +1,13 @@
+function mostraIdade(nome, idade) {
+  return `${nome} tem ${idade} anos`;
+}
+
+function mostraCidade(nome, cidade) {
+  return `${nome} mora em ${cidade}`;
+}
+
+function mostraHobby(nome, hobby) {
+  return `O hobby de ${nome} é ${hobby}`;
+}
+
+export { mostraIdade, mostraCidade, mostraHobby };
---
+++ JavaScript/modulos-js/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Neto", 19));
+console.log(mostraCidade("Neto", "Niterói"));
+console.log(mostraHobby("Neto", "Tocar violão"));
