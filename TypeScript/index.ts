491e6166acfc18dc4b311f1f370c5a5042345c3e
Neto002 <antoniogfneto92@hotmail.com>
2022-06-07 11:42:44 -0300

    update

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -1,22 +1,47 @@
-interface IAnimal {
+/*interface IUsuario {
+  id: string;
+  email: string;
+  cargo?: "funcionario" | "gerente" | "coordenador" | "supervisor";
+}
+
+function redirecione(usuario: IUsuario) {
+  if (usuario.cargo) {
+    // redirecionar(usuario.cargo)
+  }
+
+  // redirecionar para area do usuario
+}*/
+
+/*interface Cachorro {
+  readonly nome: string;
+  readonly idade: number;
+  readonly parqueFavorito?: string;
+}
+
+type ReadOnlyCachorro = {
+  readonly [K in keyof Cachorro]-?: Cachorro[K];
+};
+
+class MeuCachorro implements ReadOnlyCachorro {
   nome: string;
-  especie: "terrestre" | "aquatico" | "aereo";
-  emitirSom(): void;
+  idade: number;
+  parqueFavorito: string;
+
+  constructor(nome, idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
 }
 
-interface IFelinos extends IAnimal {
-  corPelo: string;
-}
+const cao = new MeuCachorro("Cachorro", 10);
+cao.nome = "a";*/
 
-const animal: IAnimal = {
-  nome: "Elefante",
-  especie: "terrestre",
-  emitirSom: () => "barulho de elefante",
-};
+import $ from "jquery";
 
-const felino: IFelinos = {
-  nome: "Tigre",
-  especie: "terrestre",
-  corPelo: "preto",
-  emitirSom: () => "barulho de tigre",
-};
+$.fn.extend({
+  nomeFuncao() {
+    console.log("Nome da função");
+  },
+});
+
+$("body").nomeFuncao();
