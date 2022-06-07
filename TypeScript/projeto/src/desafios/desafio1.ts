2517e8b63dffe3fa56f4f7afdeb532d4d4c7a2ad
Neto002 <antoniogfneto92@hotmail.com>
2022-06-07 17:30:52 -0300

    desafios typescript

---
+++ TypeScript/projeto/src/desafios/desafio1.ts
@@ -0,0 +1,14 @@
+let employee: { code: number; name: string } = {
+  code: 10,
+  name: "John",
+};
+
+interface Employee {
+  code: number;
+  name: string;
+}
+
+let employee2: Employee = {
+  code: 10,
+  name: "John",
+}
--- TypeScript/projeto/src/exemplos/objetos-listas.ts
+++ TypeScript/projeto/src/exemplos/objetos-listas.ts
@@ -1,48 +1,48 @@
-const pessoa = {
-  nome: "Mariana",
-  idade: 28,
-  profissao: "desenvolvedora",
-};
+// const pessoa = {
+//   nome: "Mariana",
+//   idade: 28,
+//   profissao: "desenvolvedora",
+// };
 
-enum Profissao {
-  Professora,
-  Atriz,
-  Desenvolvedora,
-  JogadoraDeFutebol,
-}
+// enum Profissao {
+//   Professora,
+//   Atriz,
+//   Desenvolvedora,
+//   JogadoraDeFutebol,
+// }
 
-interface Pessoa {
-  nome: string;
-  idade: number;
-  profissao?: Profissao;
-}
+// interface Pessoa {
+//   nome: string;
+//   idade: number;
+//   profissao?: Profissao;
+// }
 
-interface Estudante extends Pessoa {
-  materias: string[];
-}
+// interface Estudante extends Pessoa {
+//   materias: string[];
+// }
 
-const vanessa: Pessoa = {
-  nome: "Vanessa",
-  idade: 23,
-  profissao: Profissao.Desenvolvedora,
-};
+// const vanessa: Pessoa = {
+//   nome: "Vanessa",
+//   idade: 23,
+//   profissao: Profissao.Desenvolvedora,
+// };
 
-const maria: Pessoa = {
-  nome: "Maria",
-  idade: 23,
-  profissao: Profissao.Desenvolvedora,
-};
+// const maria: Pessoa = {
+//   nome: "Maria",
+//   idade: 23,
+//   profissao: Profissao.Desenvolvedora,
+// };
 
-const jessica: Estudante = {
-  nome: "Jessica",
-  idade: 23,
-  materias: ["JavaScript", "TypeScript"],
-};
+// const jessica: Estudante = {
+//   nome: "Jessica",
+//   idade: 23,
+//   materias: ["JavaScript", "TypeScript"],
+// };
 
-function listar(lista: string[]) {
-  for (const item of lista) {
-    console.log("- ", item);
-  }
-}
+// function listar(lista: string[]) {
+//   for (const item of lista) {
+//     console.log("- ", item);
+//   }
+// }
 
-listar(jessica.materias);
+// listar(jessica.materias);
