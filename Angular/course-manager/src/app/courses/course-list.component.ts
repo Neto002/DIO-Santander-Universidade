1d75c2c5c6d7aec8ab3ac321a09a715c7def45ff
Neto002 <antoniogfneto92@hotmail.com>
2022-06-08 13:32:22 -0300

    criando course.service e injeção de dependência

--- Angular/course-manager/src/app/courses/course-list.component.ts
+++ Angular/course-manager/src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -8,28 +9,9 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit {
   courses: Course[] = [];
 
+  constructor(private courseService: CourseService) {}
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular Forms',
-        imageUrl: './assets/images/forms.png',
-        price: 99.99,
-        code: '1254',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'December 2, 2019',
-      },
-      {
-        id: 2,
-        name: 'Angular HTTP',
-        imageUrl: './assets/images/http.png',
-        price: 99.99,
-        code: '1255',
-        duration: 80,
-        rating: 5,
-        releaseDate: 'December 4, 2022',
-      },
-    ];
+    this.courses = this.courseService.retrieveAll();
   }
 }
--- Angular/course-manager/src/app/courses/course.ts
+++ Angular/course-manager/src/app/courses/course.ts
@@ -1,30 +1,11 @@
 export class Course {
-  id: number;
-  name: string;
-  imageUrl: string;
-  price: number;
-  code: string;
-  duration: number;
-  rating: number;
-  releaseDate: string;
-
-  constructor(
-    id: number,
-    name: string,
-    imageUrl: string,
-    price: number,
-    code: string,
-    duration: number,
-    rating: number,
-    releaseDate: string
-  ) {
-    this.id = id;
-    this.name = name;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.code = code;
-    this.duration = duration;
-    this.rating = rating;
-    this.releaseDate = releaseDate;
-  }
+  id: number = 0;
+  name: string = '';
+  imageUrl: string = '';
+  price: number = 0;
+  code: string = '';
+  duration: number = 0;
+  rating: number = 0;
+  releaseDate: string = '';
+  description: string = ''
 }
