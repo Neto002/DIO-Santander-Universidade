d11f3dab87a578ec7e83a451ce48c09e5024cc23
Antonio Neto <antoniogfneto92@hotmail.com>
2022-07-07 16:00:29 -0300

    finalizando jpa

--- SpringFramework/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ SpringFramework/aula-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -1,5 +1,7 @@
 package dio.aula;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -14,17 +16,35 @@ public class StartApp implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+//		User user = new User();
+//		user.setName("Antonio");
+//		user.setUsername("neto002");
+//		user.setPassword("neto123");
+//		
+//		repository.save(user);
+//		
+//		for (User usr: repository.findAll()) {
+//			System.out.println(usr);
+//		}
+		
+		List<User> users = repository.filtrarPorNome("Antonio");
+		for (User u: users) {
+			System.out.println(u);
+		}
+		
+	}
+	
+	private void insertUser() {
 		User user = new User();
-		user.setName("Antonio");
-		user.setUsername("neto002");
-		user.setPassword("neto123");
+		user.setName("test");
+		user.setUsername("test");
+		user.setPassword("test");
 		
 		repository.save(user);
 		
-		for (User usr: repository.findAll()) {
-			System.out.println(usr);
+		for (User u: repository.findAll()) {
+			System.out.println(u);
 		}
-		
 	}
 
 }
