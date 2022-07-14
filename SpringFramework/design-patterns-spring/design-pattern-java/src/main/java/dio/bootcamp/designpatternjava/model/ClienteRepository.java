84a67437ab95ece6fe68c74fbcaaad0b399e09e3
Antonio Neto <antoniogfneto92@hotmail.com>
2022-07-14 11:57:45 -0300

    update

--- SpringFramework/design-patterns-spring/design-pattern-java/src/main/java/dio/bootcamp/designpatternjava/model/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package dio.bootcamp.designpatternjava.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    
-}
--- SpringFramework/design-patterns-spring/design-pattern-java/src/main/java/dio/bootcamp/designpatternjava/model/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package dio.bootcamp.designpatternjava.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnderecoRepository extends JpaRepository<Cliente, String> {
-    
-}
